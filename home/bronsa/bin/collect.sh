7ea8bbcc87a30c92acd3cf006a9b649cb5180e30
Bronsa <brobronsa@gmail.com>
2010-08-11 14:11:26 +0200

    NEW STUFF

--- home/bronsa/bin/collect.sh
+++ home/bronsa/bin/collect.sh
@@ -4,10 +4,10 @@ while true ; do
 	CPUTEMP=$(sh /home/bronsa/bin/cputemp.sh)
 	GPUTEMP=$(aticonfig --adapter=0 --od-gettemperature | tail -n 1 | cut -c 36-38 | head -c-1)
 	LOADAVG=$(uptime | tail -c17 | cut -c 1-4 | head -c-1)
-	NETIN=$(cat /tmp/net.in)
-	NETOUT=$(cat /tmp/net.out)
+#	NETIN=$(cat /tmp/net.in)
+#	NETOUT=$(cat /tmp/net.out)
 	UNAME=$(uname -r | head -c-1)
-	FILE="(:|CPULOAD| \"${CPULOAD}\" :|CPUTEMP| \"${CPUTEMP}\" :|GPUTEMP| \"${GPUTEMP}\" :|LOADAVG| \"${LOADAVG}\" :|NETIN| \"${NETIN}\" :|NETOUT| \"${NETOUT}\" :|UNAME| \"${UNAME}\")"
+	FILE="(:|CPULOAD| \"${CPULOAD}\" :|CPUTEMP| \"${CPUTEMP}\" :|GPUTEMP| \"${GPUTEMP}\" :|LOADAVG| \"${LOADAVG}\" :|UNAME| \"${UNAME}\")"
 	echo -en "\n${FILE}" > /tmp/data
 	sleep 0.5
 done
--- home/bronsa/bin/mode-line-info.sh
+++ home/bronsa/bin/mode-line-info.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 uname -r | head -c-1 > /tmp/uname
 sh /home/bronsa/bin/cpu.sh &
-/home/bronsa/bin/net.sh 1> /dev/null &
 while true ; do
 	sh /home/bronsa/bin/collect.sh
 	sleep 1
